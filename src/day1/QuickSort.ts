c2a3bd14fc9804de1ab195e1767bca493fdf8a8e
xxxx <xxxx>
2024-05-09 22:23:26 +0200

    wheeeee (qs)

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,25 +1,31 @@
 export default function quick_sort(arr: number[]): void {
-    arr = quick_rec(arr);
+    
+    quick_rec(arr, 0, arr.length, 0);
+
+
 }
 
-function quick_rec(arr: number[]): number[] {
-    if (arr.length<2) {
-        return arr;
+function quick_rec(arr: number[], min: number, max: number, depth: number): void {
+    if (min>=max) {
+        return
     }
 
-    let pivot = arr[arr.length-1];
-    let left = []
-    let right = []
-    for (let i =0; i<arr.length; i++){
-        if (arr[i]<pivot) {
-            left.push(arr[i])
-        } else if (arr[i]>pivot) {
-            right.push(arr[i])
+    var pivotVal = arr[max-1]
+    var pivotIndex = min-1
+    for (var i = min; i<max-1; i++) {
+        if ( arr[i]<pivotVal ) {
+            pivotIndex++
+            var temp = arr[pivotIndex]
+            arr[pivotIndex] = arr[i]
+            arr[i] = temp
         }
     }
+    pivotIndex++
+    var temp = arr[pivotIndex]
+    arr[pivotIndex] = pivotVal
+    arr[max-1] = temp
 
-    left = quick_rec(left)
-    right = quick_rec(right)
-    
-    return left.concat([pivot]).concat(right)
+    quick_rec(arr, min, pivotIndex, depth+1)
+    quick_rec(arr, pivotIndex+1, max, depth+1)
+
 }
